93a5e78869d837aebe56cc7060b991e5bb4a0562
jonychen <jony.k.chen@gmail.com>
2026-04-13 18:30:34 +0800

    feat: 添加 LangChain4j Spring Boot 项目骨架

---
+++ src/main/java/com/jonychen/AiApplication.java
@@ -0,0 +1,12 @@
+package com.jonychen.model;
+
+/**
+ * @author 30240
+ */
+@SpringBootApplication
+public class AiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiApplication.class, args);
+    }
+}
